83b58973a49a9ca93944367fdeda86eb6321eab4
christian <horpeyemijoshua@gmail.com>
2022-09-08 22:48:09 +0100

    prints to the screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+*main - program that prints to the screen
+*
+* return  0;
+*/
+int main()
+{
+    puts("Programming is like building a multilingual puzzle\n");
+
+    return 0;
+}
