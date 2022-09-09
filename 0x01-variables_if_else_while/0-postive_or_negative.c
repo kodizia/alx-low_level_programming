c62a72daa5cc97768f288c94e8ecbe5c989fcd38
christian <horpeyemijoshua@gmail.com>
2022-09-09 23:40:36 +0100

    check if a number is +ve of -ve

---
+++ 0x01-variables_if_else_while/0-postive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - check if number is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive", n);
+if (n < 0)
+printf("%d is negative", n);
+else
+printf("%d is zero", n);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+these tasks are for variables if and else statements
