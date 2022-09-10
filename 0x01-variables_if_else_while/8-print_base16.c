d83cd6700ed0a3c3b59a9e05cc4922a5c30c7861
christian <horpeyemijoshua@gmail.com>
2022-09-10 02:19:33 +0100

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int var;
 int var1;
 for (var = 0; var < 10; var++)
 putchar((var % 10) + '0');
-for (var1 = 'a'; var1 < 'f'; var1++)
+for (var1 = 'a'; var1 <='f'; var1++)
 putchar(var1);
 putchar('\n');
 return (0);
