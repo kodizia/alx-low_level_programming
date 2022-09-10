0f48830df2343a172a08be25f02ca426d8b8f502
christian <horpeyemijoshua@gmail.com>
2022-09-10 02:18:28 +0100

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program that prints hex numbers
+ *Return: 0 if succesful
+ */
+int main(void)
+{
+int var;
+int var1;
+for (var = 0; var < 10; var++)
+putchar((var % 10) + '0');
+for (var1 = 'a'; var1 < 'f'; var1++)
+putchar(var1);
+putchar('\n');
+return (0);
+}
