e5cccf5fcf42fffc92abfaed5846e903eb5d7181
christian <horpeyemijoshua@gmail.com>
2022-09-14 00:20:33 +0100

    code fixing

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,8 +4,7 @@
  */
 void jack_bauer(void)
 {
-int hour;
-int min;
+int hour, min;
 for (hour = 0; hour < 24; hour++)
 {
 for (min = 0; min < 60; min++)
@@ -14,7 +13,7 @@ _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((min / 10) + '0');
-_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
 _putchar('\n');
 }
 }
