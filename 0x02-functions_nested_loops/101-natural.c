3ab6ab6515e72428f025cbddaf9926d91cd9514a
christian <horpeyemijoshua@gmail.com>
2022-09-14 01:28:38 +0100

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - find sum of multiples of 3 and 5 less than 1024
+ *
+ * Created by: christian joshua
+ * cc: 14th september
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+int ii, _sum;
+
+for (ii = 3; ii < 1024; ii++)
+{
+if (ii % 3 == 0 || ii % 5 == 0)
+_sum += ii;
+}
+printf("%d\n", _sum);
+return (0);
+}
