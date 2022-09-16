a5dc59fc3b756824158925d7de3b1d925abab68e
christian <horpeyemijoshua@gmail.com>
2022-09-16 21:49:55 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_numbers - unction that prints the numbers
+ */
+void print_numbers(void)
+{
+int i = 0;
+for (i = 0; i < 10; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
