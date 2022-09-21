53cbdef3deaa85bfa7f8b3b11daa35483f0f04a0
christian <horpeyemijoshua@gmail.com>
2022-09-21 01:47:07 +0100

    array

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - print the last half of a string
+ * @s: pointer to the string variable s
+ *
+ */
+void puts_half(char *s)
+{
+int len = strlen(s), p;
+if (len % 2 == 0)
+{
+for (p = len / 2; p < len; p++)
+_putchar(s[p]);
+}
+else
+for (p = (len / 2) + 1; p < len; p++)
+_putchar(s[i]);
+_putchar('\n');
+}
