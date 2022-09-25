47c764d0dcb1545aeb0d6f9e5dc3b1d4ea69e648
christian <horpeyemijoshua@gmail.com>
2022-09-25 13:28:03 +0100

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,16 @@
 #include <string.h>
 #include "main.h"
+
 /**
- * _strcmp - compare 2 strings
- *@s1:pointer to the first string
- *@s2:pointer to the second string
- *Return:0
+ * _strcmp - compare  strings together
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ *
+ * Return: int
+ *
  */
 
 int _strcmp(char *s1, char *s2)
 {
-strcmp(s1, s2);
-return (0);
+return (strcmp(s1, s2));
 }
