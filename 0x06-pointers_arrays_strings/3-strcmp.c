8eff2083f3fffadc9d3927862ba3427158187259
christian <horpeyemijoshua@gmail.com>
2022-09-25 12:55:05 +0100

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcmp- function to compare string
+ * 
+ * @param s1: first string 
+ * @param s2: pointer to second string
+ * Return: 0;
+ */
+int _strcmp(char *s1, char *s2);
+{
+_strcmp(s1, s2);
+return (0);
+}
