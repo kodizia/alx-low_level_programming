8904ebc2d5c05afd44fb6763cefad4a55c70b8e4
christian <horpeyemijoshua@gmail.com>
2022-09-26 20:05:51 +0100

    fixing code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 int size = n; /*accept any positive integer*/
+
 if (size > 0)
 {
 int i;
-for (i = 0; i < size;i++)
+for (i = 0; i < size; i++)
 s[i] = b;
 }
 return (s);
